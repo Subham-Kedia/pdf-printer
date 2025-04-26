7e1e46df2f51b7b0dbd277dc3a77059f6d8990ff
Shubham Kedia <shubham.kedia@vegrow.in>
2025-04-26 22:00:17 +0530

    printer logic modified

--- android/src/main/java/com/vegrow/plugins/pdfprinter/PdfPrinterPlugin.java
+++ android/src/main/java/com/vegrow/plugins/pdfprinter/PdfPrinterPlugin.java
@@ -15,7 +15,7 @@ public class PdfPrinterPlugin extends Plugin {
     @PluginMethod
     public void printPDF(PluginCall call) {
         String url = call.getString("url");
-        String paperType = call.getString("paper_type", "ISO_A4"); // Default to A4 if not specified
+        String paper_type = call.getString("paper_type", "ISO_A4"); // Default to A4 if not specified
 
         if (url == null || !URLUtil.isValidUrl(url)) {
             call.reject("Invalid or missing PDF URL");
